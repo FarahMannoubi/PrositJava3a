0a2907e7a8aef725acfe7ce64eb53b27736423e6
macbook <farah.mannoubi@esprit.tn>
2024-10-26 15:34:22 -0700

    feat:to do prosit2

--- Zoo/src/ZooManagement.java
+++ Zoo/src/ZooManagement.java
@@ -1,11 +1,25 @@
 public class ZooManagement {
 
-    int nbrCages = 20;
-    String zooName = "My Zoo";
+    public static void main(String[] args) {
+        Animal lion = new Animal();
+        lion.name = "Simba";
+        lion.age = 8;
+        lion.family = "Cats";
+        lion.isMammal = true;
+
+        Zoo myZoo = new Zoo("Wildlife Park", "Ariana",25);
+        Zoo notMyZoo = new Zoo("WaterPark", "Siliana",25);
 
 
+        Animal dog = new Animal("Canine", "Snoopy", 2, true);
 
-    void displayInformation(){
-        System.out.println(zooName + " has " + nbrCages + " cages");
+
+        myZoo.displayZoo();
+
+        System.out.println(myZoo);
+        System.out.println(dog);
+
+
     }
+
 }
